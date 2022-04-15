c88058cfc709b011f6d3d95aa647f595304133ea
Lukas Ruegner <ruegnerlukas@gmail.com>
2022-04-15 18:29:39 +0200

    add backend websockets hello-world

--- strategy-game-backend/src/main/kotlin/de/ruegnerlukas/strategygame/backend/Application.kt
+++ strategy-game-backend/src/main/kotlin/de/ruegnerlukas/strategygame/backend/Application.kt
@@ -6,6 +6,8 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
 
 fun main() {
 	embeddedServer(
@@ -15,6 +17,7 @@ fun main() {
 		watchPaths = listOf("classes")
 	) {
 		install(Routing)
+		install(WebSockets)
 		configureRouting()
 	}.start(wait = true)
 }
@@ -26,6 +29,23 @@ fun Application.configureRouting() {
 		}
 		get("/hello/{name}") {
 			call.respond(HttpStatusCode.OK, "Hello ${call.parameters["name"]}!")
+		}
+		webSocket("/echo") {
+			send(Frame.Text("Please enter your name"))
+			for (frame in incoming) {
+				when (frame) {
+					is Frame.Text -> {
+						val received = frame.readText()
+						println("Received: $received")
+						if (received.equals("bye", ignoreCase = true)) {
+							close(CloseReason(CloseReason.Codes.NORMAL, "Client said bye"))
+						} else {
+							send(Frame.Text("Hi, $received"))
+						}
+					}
+					else -> print("Unknown frame-type: $frame")
+				}
+			}
 		}
 	}
 }
