def5c0914bb3549c7c8624fa001aacfd953db0c2
Lukas Ruegner <ruegnerlukas@gmail.com>
2022-04-16 12:11:37 +0200

    frontend env vars

--- strategy-game-frontend/src/client/client.ts
+++ strategy-game-frontend/src/client/client.ts
@@ -1,9 +1,7 @@
-import {Simulate} from "react-dom/test-utils";
 
 export namespace Client {
 
-	const BASE_URL = "http://localhost:8080";
-	const WS_BASE_URL = "ws://localhost:8080";
+	const BASE_URL = import.meta.env.PUB_BACKEND_URL;
 
 	export function getHello(name: string): Promise<string> {
 		return fetch(`${BASE_URL}/hello/${name}`)
