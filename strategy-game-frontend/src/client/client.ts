3a5d484c3455cf7f9be19dc654e454aeba1fda80
Lukas Ruegner <ruegnerlukas@gmail.com>
2022-04-17 18:04:21 +0200

    create game screen

--- strategy-game-frontend/src/client/client.ts
+++ strategy-game-frontend/src/client/client.ts
@@ -1,12 +1,47 @@
-
 export namespace Client {
 
 	const BASE_URL = import.meta.env.PUB_BACKEND_URL;
 
+
 	export function getHello(name: string): Promise<string> {
-		return fetch(`${BASE_URL}/hello/${name}`)
+		return get(`${BASE_URL}/api/test/hello/${name}`)
 			.then(response => response.text())
 			.catch(() => "Error");
+	}
+
+
+	export interface WorldMeta {
+		worldId: string;
+	}
+
+
+	export function createWorld(): Promise<WorldMeta> {
+		return post(`${BASE_URL}/api/world/create`)
+			.then(response => response.json())
+			.then(data => ({worldId: data.worldId}))
+			.catch(() => {
+				throw new Error("Error creating world");
+			});
+	}
+
+
+	function get(url: string): Promise<Response> {
+		return fetch(url);
+	}
+
+
+	function post(url: string, body?: any): Promise<Response> {
+		return fetch(url, {
+				method: "POST",
+				headers: {
+					"Accept": "application/json",
+					"Content-Type": "application/json"
+				},
+				body: (body !== undefined && body != null)
+					? JSON.stringify(body, null, "   ")
+					: undefined
+			}
+		);
 	}
 
 }
--- strategy-game-frontend/src/ui/App.tsx
+++ strategy-game-frontend/src/ui/App.tsx
@@ -1,27 +1,14 @@
-import "./App.css";
-import {useState} from "react";
-import {Client} from "../client/client";
+import {CreateWorld} from "./createworld/CreateWorld";
+import "./app.css";
 
 export function App() {
 
-	const [text, setText] = useState("");
-	const [response, setResponse] = useState("");
-
 	return (
 		<div className="app">
-			<b>Hello-World Rest<br/></b>
-			<input type="text" value={text} onChange={onChangeText}/>
-			<button onClick={onSubmit}>Submit</button>
-			<div className="response">{response}</div>
+			<div className="app-content">
+				<CreateWorld/>
+			</div>
 		</div>
 	);
-
-	function onChangeText(e: any) {
-		setText(e.target.value);
-	}
-
-	function onSubmit() {
-		Client.getHello(text).then(setResponse);
-	}
 
 }
