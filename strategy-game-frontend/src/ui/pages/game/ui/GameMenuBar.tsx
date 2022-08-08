879ef5a7b0a66c0a6303890b11ae9110993ce137
Lukas Ruegner <ruegnerlukas@gmail.com>
2022-08-08 23:47:01 +0200

    display money in game-ui

--- strategy-game-frontend/src/ui/pages/game/ui/GameMenuBar.tsx
+++ strategy-game-frontend/src/ui/pages/game/ui/GameMenuBar.tsx
@@ -2,6 +2,7 @@ import {ReactElement} from "react";
 import "./gameMenuBar.css";
 import {GameStateHooks} from "../../../../external/state/game/gameStateHooks";
 import {UserStateHooks} from "../../../../external/state/user/userStateHooks";
+import {Country} from "../../../../models/state/country";
 import {CategoryDebug} from "./MenuDebug";
 import {CategoryOther} from "./MenuOther";
 import {NextTurnAction} from "./NextTurnAction";
@@ -20,10 +21,7 @@ export function GameMenuBar(): ReactElement {
                 <CategoryDebug/>
             </div>
             <div className="info-section">
-                <FoodResource/>
-                <WoodResource/>
-                <StoneResource/>
-                <MetalResource/>
+                <MoneyResource country={country}/>
             </div>
             <div className="action-section">
                 <NextTurnAction/>
@@ -32,21 +30,10 @@ export function GameMenuBar(): ReactElement {
     );
 }
 
-
-export function FoodResource(): ReactElement {
-    return (<div>F: 298/400 (+4)</div>);
+export function MoneyResource(props: {country: Country | undefined}): ReactElement {
+    if(props.country) {
+        return (<div>{"Money: " + props.country!!.resources.money}</div>);
+    } else {
+        return <div></div>
+    }
 }
-
-export function WoodResource(): ReactElement {
-    return (<div>W: 4503/6000 (+24)</div>);
-}
-
-export function StoneResource(): ReactElement {
-    return (<div>S: 280/300 (+3)</div>);
-}
-
-export function MetalResource(): ReactElement {
-    return (<div>M: 59/100 (-2)</div>);
-}
-
-
