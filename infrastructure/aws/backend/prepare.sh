569dd5e860eca1be0c548f43292b184ada1d74bb
Lukas Ruegner <ruegnerlukas@gmail.com>
2022-11-01 20:43:46 +0100

    test prod ssl-cert

--- infrastructure/aws/backend/prepare.sh
+++ infrastructure/aws/backend/prepare.sh
@@ -5,3 +5,5 @@ set -e
 aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 627717213620.dkr.ecr.eu-central-1.amazonaws.com
 sudo docker pull 627717213620.dkr.ecr.eu-central-1.amazonaws.com/strategy-game:latest
 sudo docker tag 627717213620.dkr.ecr.eu-central-1.amazonaws.com/strategy-game:latest strategy-game:latest
+
+aws s3 cp s3://strategy-game.config/certificate/keystore.jks /home/ubuntu/server/infrastructure/sslCert/keystore.jks
