8e26f736e44f84d3b679870ed716522545d8bf1a
Lukas Ruegner <ruegnerlukas@gmail.com>
2023-08-04 11:24:21 +0200

    wip: component restructuring

---
+++ frontend/src/ui/components/panels/decorated/PanelDecorated.tsx
@@ -0,0 +1,21 @@
+import {ReactElement} from "react";
+import "./decoratedPanel.css";
+import {joinClassNames} from "../../utils";
+
+
+export interface PanelDecoratedProps {
+    children?: any;
+    className?: string;
+}
+
+export function PanelDecorated(props: PanelDecoratedProps): ReactElement {
+
+    return (
+        <div className={joinClassNames(["decorated-panel", props.className])}>
+            <div className="decorated-panel__background"/>
+            <div className="decorated-panel__border"/>
+            {props.children}
+        </div>
+    );
+
+}
