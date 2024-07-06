387763fe4393c6025c30af1969ad625fa4b94cd0
Lukas Ruegner <ruegnerlukas@gmail.com>
2024-07-06 22:10:23 +0200

    test arangodb

--- backend/strategy-game-common-arangodb/build.gradle.kts
+++ backend/strategy-game-common-arangodb/build.gradle.kts
@@ -27,6 +27,15 @@ dependencies {
     val versionKotlinLogging: String by project
     implementation("io.github.microutils:kotlin-logging-jvm:$versionKotlinLogging")
 
+    val versionKotest: String by project
+    val versionKotestExtensionTestContainers: String by project
+    testImplementation("io.kotest:kotest-runner-junit5:$versionKotest")
+    testImplementation("io.kotest:kotest-assertions-core:$versionKotest")
+    testImplementation("io.kotest:kotest-property:$versionKotest")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:$versionKotestExtensionTestContainers")
+
+    val versionMockk: String by project
+    testImplementation("io.mockk:mockk:${versionMockk}")
 }
 
 kotlin {
---
+++ backend/strategy-game-common-arangodb/src/test/kotlin/io/github/smiley4/strategygame/backend/commonarangodb/TestContainers.kt
@@ -0,0 +1,35 @@
+package io.github.smiley4.strategygame.backend.commonarangodb
+
+import io.kotest.core.config.AbstractProjectConfig
+import org.slf4j.LoggerFactory
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.output.Slf4jLogConsumer
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy
+import org.testcontainers.utility.DockerImageName
+import java.time.Duration
+
+class TestContainers : AbstractProjectConfig() {
+
+    companion object {
+        var arangoDbPort: Int? = null
+    }
+
+    override suspend fun beforeProject() {
+        arangoDbPort = ArangoDbContainer.start().getMappedPort(8529)
+    }
+}
+
+
+internal object ArangoDbContainer {
+
+    fun start(): GenericContainer<*> {
+        val container = GenericContainer(DockerImageName.parse("arangodb:3.10.0"))
+            .withEnv("ARANGO_NO_AUTH", "1")
+            .withExposedPorts(8529)
+            .waitingFor(HttpWaitStrategy().forPort(8529).withStartupTimeout(Duration.ofMinutes(5)))
+            .withLogConsumer(Slf4jLogConsumer(LoggerFactory.getLogger(ArangoDbContainer::class.java)))
+        container.start()
+        return container
+    }
+
+}
