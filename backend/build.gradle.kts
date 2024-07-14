93b1d31d6f8c6898f77e8e84a33afece0bced357
Lukas Ruegner <ruegnerlukas@gmail.com>
2024-07-14 17:57:22 +0200

    optimize gradle build

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
     kotlin("plugin.serialization") version "1.9.21" apply false
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
 }
+
+
+extensions.findByName("buildScan")?.withGroovyBuilder {
+    setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+    setProperty("termsOfServiceAgree", "yes")
+}
