21aacb5ccda517f52c8fa21de3e3b7645ed1e21b
Lukas Ruegner <lukas.ruegner@xitaso.com>
2024-12-28 14:44:00 +0100

    minor world object window improvements

--- frontend/src/ui/pages/ingame/windows/worldobject/WorldObjectWindow.tsx
+++ frontend/src/ui/pages/ingame/windows/worldobject/WorldObjectWindow.tsx
@@ -1,15 +1,16 @@
 import React, {ReactElement} from "react";
 import {VBox} from "../../../../components/layout/vbox/VBox";
 import {Text} from "../../../../components/text/Text";
-import {WindowSection} from "../../../../components/section/ContentSection";
-import {InsetKeyValueGrid} from "../../../../components/keyvalue/KeyValueGrid";
-import {EnrichedText} from "../../../../components/textenriched/EnrichedText";
 import {UseWorldObjectWindow} from "./useWorldObjectWindow";
 import {ButtonPrimary} from "../../../../components/button/primary/ButtonPrimary";
 import {Else, If, Then, When} from "react-if";
 import {DecoratedWindow} from "../../../../components/window/decorated/DecoratedWindow";
 import {Banner} from "../../../../components/banner/Banner";
 import {FiHexagon} from "react-icons/fi";
+import {InsetPanel} from "../../../../components/panels/inset/InsetPanel";
+import {InsetKeyValueGrid} from "../../../../components/keyvalue/KeyValueGrid";
+import {EnrichedText} from "../../../../components/textenriched/EnrichedText";
+import {Spacer} from "../../../../components/spacer/Spacer";
 
 export interface WorldObjectWindowProps {
     windowId: string;
@@ -44,45 +45,69 @@ export function WorldObjectWindow(props: WorldObjectWindowProps): ReactElement {
                         </ButtonPrimary>
                     </Banner>
 
-                    <VBox scrollable fillParent gap_s stableScrollbar top stretch padding_m>
+                    <VBox scrollable gap_s stableScrollbar top stretch padding_m>
 
-                        <WindowSection>
-                            <InsetKeyValueGrid>
+                        <Text>Available Actions:</Text>
 
-                                <EnrichedText>Id</EnrichedText>
-                                <EnrichedText>{data.worldObject.id}</EnrichedText>
+                        <InsetPanel>
 
-                                <EnrichedText>Position</EnrichedText>
-                                <EnrichedText>{data.worldObject.tile.q + ", " + data.worldObject.tile.r}</EnrichedText>
+                            <VBox gap_s top stretch>
 
-                                <EnrichedText>Country</EnrichedText>
-                                <EnrichedText>{data.worldObject.country.name}</EnrichedText>
+                                <When condition={data.movement.possible}>
+                                    <If condition={data.movement.canCancel}>
+                                        <Then>
+                                            <ButtonPrimary info onClick={data.movement.cancel}>
+                                                Cancel Movement
+                                            </ButtonPrimary>
+                                        </Then>
+                                        <Else>
+                                            <ButtonPrimary info onClick={data.movement.start}
+                                                           disabled={!data.movement.enabled}>
+                                                Move
+                                            </ButtonPrimary>
+                                        </Else>
+                                    </If>
+                                </When>
 
-                            </InsetKeyValueGrid>
-                        </WindowSection>
-
-                        <When condition={data.movement.possible}>
-                            <If condition={data.movement.canCancel}>
-                                <Then>
-                                    <ButtonPrimary info onClick={data.movement.cancel}>
-                                        Cancel Movement
+                                <When condition={data.settlement.possible}>
+                                    <ButtonPrimary info onClick={data.settlement.start}
+                                                   disabled={!data.settlement.enabled}>
+                                        Found Settlement
                                     </ButtonPrimary>
-                                </Then>
-                                <Else>
-                                    <ButtonPrimary info onClick={data.movement.start} disabled={!data.movement.enabled}>
-                                        Move
-                                    </ButtonPrimary>
-                                </Else>
-                            </If>
-                        </When>
+                                </When>
 
-                        <When condition={data.settlement.possible}>
-                            <ButtonPrimary info onClick={data.settlement.start} disabled={!data.settlement.enabled}>
-                                Found Settlement
-                            </ButtonPrimary>
-                        </When>
+                            </VBox>
+
+                        </InsetPanel>
+
+                        <Spacer size={"s"}/>
+
+                        <InsetKeyValueGrid>
+                            <EnrichedText>Id</EnrichedText>
+                            <EnrichedText>{data.worldObject.id}</EnrichedText>
+                        </InsetKeyValueGrid>
 
                     </VBox>
+
+                    {/*<VBox scrollable fillParent gap_s stableScrollbar top stretch padding_m>*/}
+
+                    {/*    <WindowSection>*/}
+                    {/*        <InsetKeyValueGrid>*/}
+
+                    {/*            <EnrichedText>Id</EnrichedText>*/}
+                    {/*            <EnrichedText>{data.worldObject.id}</EnrichedText>*/}
+
+                    {/*            <EnrichedText>Position</EnrichedText>*/}
+                    {/*            <EnrichedText>{data.worldObject.tile.q + ", " + data.worldObject.tile.r}</EnrichedText>*/}
+
+                    {/*            <EnrichedText>Country</EnrichedText>*/}
+                    {/*            <EnrichedText>{data.worldObject.country.name}</EnrichedText>*/}
+
+                    {/*        </InsetKeyValueGrid>*/}
+                    {/*    </WindowSection>*/}
+
+
+                    {/*</VBox>*/}
                 </VBox>
             </DecoratedWindow>
         );
