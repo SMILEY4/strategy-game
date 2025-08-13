3d98abf4bfe2a4b7854d916de53e8663bd87584c
Lukas Ruegner <ruegnerlukas@gmail.com>
2025-08-13 23:57:23 +0200

    setup wasm

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import {App} from "./ui/pages/App";
 import {ArrayExtensions} from "./common/extensions";
 
+import * as wasm from "wasm"
+
+wasm.greet()
+
 ArrayExtensions.setup()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App/>);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,12 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import {reactScopedCssPlugin} from "rollup-plugin-react-scoped-css";
-
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     // @ts-ignore
-    plugins: [react(), reactScopedCssPlugin()],
+    plugins: [react(), reactScopedCssPlugin(), wasm()],
     envDir: "env",
     envPrefix: "PUB_",
 });
