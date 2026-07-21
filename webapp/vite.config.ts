de22413edcc00dda67ab28f9466848c3e817ce1e
Lukas Ruegner <ruegnerlukas@gmail.com>
2026-07-21 09:44:11 +0200

    setup wasm

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm()],
   resolve: {
     alias: {
       "@": "/src",
