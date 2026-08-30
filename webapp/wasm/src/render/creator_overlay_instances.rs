0d37ad0c2fe49dbb0e88f97896edf3c5568b6167
Lukas Ruegner <ruegnerlukas@gmail.com>
2026-08-30 21:28:59 +0200

    wip, overlay rendering

--- webapp/wasm/src/render/creator_overlay_instances.rs
+++ webapp/wasm/src/render/creator_overlay_instances.rs
@@ -1,12 +1,10 @@
-use crate::js::models::HexPosition;
+use std::collections::HashMap;
+use crate::js::models::{
+    HexPosition, Tile, TILE_VISIBILITY_UNDISCOVERED,
+};
 use crate::render::models::render_state::RenderState;
-use crate::render::models::tile_instance_data::{GridOverlayInstance, OverlayVertexData};
+use crate::render::models::tile_instance_data::{GenericEdgeOverlayInstance, GenericFillOverlayInstance, GridOverlayInstance, OverlayVertexData};
 
-// render grid centered on cursor
-//      grid instances centered on 0,0 with radius r
-//      render with uniform == cursor position
-//      vert-shader moves grid to tile with cursor
-//      frag-shader calculates distance -> fades edges of grid
 pub fn build_tile_grid(state: &RenderState, instance_data: &mut OverlayVertexData) {
     instance_data.grid_instances.clear();
     HexPosition { q: 0, r: 0 }.iterate_circle(3, |pos| {
@@ -15,3 +13,37 @@ pub fn build_tile_grid(state: &RenderState, instance_data: &mut OverlayVertexDat
         })
     })
 }
+
+pub fn build_overlay(
+    state: &RenderState,
+    instance_data: &mut OverlayVertexData,
+    create_fill_func: impl Fn(&RenderState, &Tile) -> Vec<GenericFillOverlayInstance>,
+    create_edge_func: impl Fn(&RenderState, &Tile, &HashMap<HexPosition, usize>) -> Vec<GenericEdgeOverlayInstance>,
+) {
+    instance_data.fill_instances.clear();
+    instance_data.edge_instances.clear();
+
+    let mut tiles_by_pos = HashMap::with_capacity(state.tiles.len());
+    for (index, tile) in state.tiles.iter().enumerate() {
+        tiles_by_pos.insert(tile.tile_position, index);
+    }
+
+    state.visible_chunks.iter().for_each(|chunk_key| {
+        let chunk = state.chunks.get(chunk_key).unwrap();
+        chunk.tiles.iter().for_each(|tile_index| {
+            let tile = state.tiles[*tile_index];
+
+            if tile.visibility == TILE_VISIBILITY_UNDISCOVERED {
+                return;
+            }
+
+            let fills = create_fill_func(&state, &tile);
+            instance_data.fill_instances.extend(fills);
+
+            let edges = create_edge_func(&state, &tile, &tiles_by_pos);
+            instance_data.edge_instances.extend(edges);
+
+        })
+    });
+}
+
--- webapp/wasm/src/render/models/tile_instance_data.rs
+++ webapp/wasm/src/render/models/tile_instance_data.rs
@@ -45,11 +45,30 @@ pub struct MapDetailVertex {
 
 #[derive(Default)]
 pub struct OverlayVertexData {
-    pub grid_instances: Vec<GridOverlayInstance>
+    pub grid_instances: Vec<GridOverlayInstance>,
+    pub fill_instances: Vec<GenericFillOverlayInstance>,
+    pub edge_instances: Vec<GenericEdgeOverlayInstance>
 }
 
 #[repr(C, packed)]
 #[derive(Debug, Clone, Copy)]
 pub struct GridOverlayInstance {
     pub position: [f32; 2],
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Clone, Copy)]
+pub struct GenericFillOverlayInstance {
+    pub position: [f32; 2],
+    pub color: [f32; 4],
+    pub style: u32
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Clone, Copy)]
+pub struct GenericEdgeOverlayInstance {
+    pub position: [f32; 2],
+    pub direction: u32,
+    pub color: [f32; 4],
+    pub style: u32
 }
